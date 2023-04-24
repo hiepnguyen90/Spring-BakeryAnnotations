374888cf2ef49b6bc4683a0a267d9d7f114066b2
hiepnguyen90 <johnhiepnguyen1@gmail.com>
2023-04-24 17:57:14 -0400

    done

--- src/main/java/com/zipcodewilmington/bakery/models/Baker.java
+++ src/main/java/com/zipcodewilmington/bakery/models/Baker.java
@@ -3,7 +3,16 @@ package com.zipcodewilmington.bakery.models;
 
 import java.util.Objects;
 
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Baker {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     private String name;
@@ -13,6 +22,8 @@ public class Baker {
     private String specialty;
 
     public Baker() {
+
+        
     }
 
     public Baker(String name, String employeeId, String specialty) {
